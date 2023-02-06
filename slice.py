96300d5935fbd95980195cbc95eed56a34430a15
Steven McCulloch <stmcculloch@gmail.com>
2023-02-05 20:25:39 -0800

    made slicing much longer but at least it works

--- slice.py
+++ slice.py
@@ -1,3 +1,5 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits import mplot3d
@@ -9,11 +11,13 @@ from stl import mesh
 
 from util import *
 
-LAYER_HEIGHT = 0.4
+start_time = time.time()
+LAYER_HEIGHT = 0.999
+LINE_WIDTH = 1
 WALLS = 2
 PLOT_OUTPUT = True
 # Load the STL file into memory
-your_mesh = mesh.Mesh.from_file('test-objects/Organizer.stl')
+your_mesh = mesh.Mesh.from_file('test-objects/3DBenchy.stl')
 
 # Create a plot
 fig = plt.figure()
@@ -31,51 +35,25 @@ plane_normal = np.array([0, 0, 1])
 
 # Optimize
 tris, normals = sort_tris(your_mesh, layers)
+time_to_optimize = time.time() - start_time
 
 # Slice
 for layer, z_slice in enumerate(layers):
-    print(f"layer {layer}, {z_slice}")
+    print(f"layer {layer}, z = {z_slice}")
     
     # Move the slicing plane to this specific Z height
     plane_point = np.array([0, 0, z_slice])
     
     # Get the polygon representation of the cross-section
-    #cross_section = polygonize_full(slice_stl(buckets[layer], plane_normal, plane_point))
-    polygons, cuts, dangles, invalids = polygonize_full(slice_stl(tris[layer], normals[layer], plane_normal, plane_point).geoms)
+    polygons = slice_stl(tris[layer], normals[layer], plane_normal, plane_point)
+    for poly in polygons.geoms:
+        x, y = poly.exterior.xy
+        ax.plot(x, y, z_slice, color='red', linewidth=0.25)
 
-    wall_polys = []
-    for i in range(WALLS):
-        # TODO: positive buffer for holes
-        wall_polys.append(polygons.buffer(-i/4))
-    
-    wall_polys = GeometryCollection(wall_polys)
-    
-    # Plot the polygon
-    if PLOT_OUTPUT:   
-        if wall_polys.geom_type == 'Polygon':
-            x, y = polygons.coords.xy
-            ax.plot(x, y, z_slice, color='green')
-        for dangle in dangles.geoms:
-            x, y = dangle.coords.xy
-            ax.plot(x, y, z_slice, color='gray')
-        else:    
-            outer_wall = True
-            for geom in wall_polys.geoms:
-                if geom.geom_type == 'Polygon':
-                    x, y = geom.exterior.coords.xy
-                    if outer_wall:
-                        ax.plot(x, y, z_slice, color='red', linewidth=0.25)
-                        outer_wall = False
-                    else:
-                        ax.plot(x, y, z_slice, color='green', linewidth=0.25)
-                else:    
-                    for geom1 in geom.geoms:
-                        x, y = geom1.exterior.coords.xy
-                        if outer_wall:
-                            ax.plot(x, y, z_slice, color='red', linewidth=0.25)
-                            outer_wall = False
-                        else:
-                            ax.plot(x, y, z_slice, color='green', linewidth=0.25)
+# Timing how long the program takes
+time_to_slice = time.time() - start_time - time_to_optimize
+print(f"{time_to_optimize:.2f} seconds to optimize\n{time_to_slice:.2f} seconds to slice\n{time_to_slice / len(layers):.2f} seconds per layer\n")
+
 if PLOT_OUTPUT: 
     set_axes_equal(ax)
     plt.show()
--- util.py
+++ util.py
@@ -1,7 +1,8 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import shapely
 from shapely.geometry import (GeometryCollection, LineString, MultiLineString,
-                              Point, Polygon)
+                              MultiPolygon, Point, Polygon)
 
 
 def set_axes_equal(ax):
@@ -49,39 +50,140 @@ def slice_stl(tris, normals, plane_normal, plane_point):
     polygon_edges = []
     for i, (v1, v2, v3) in enumerate(tris):
         
-        # Edges are now vectors
-        edges = np.array([v2 - v1, v3 - v2, v1 - v3])
-        verts = np.array([v1, v2, v3])
-       
-        # Find the intersections between the triangles and the plane
-        intersections = []
+        
+        edges = np.array([v2 - v1, v3 - v2, v1 - v3]) # 3 vectors
+        verts = np.array([v1, v2, v3]) # 3 points
+        new_edge = []
+        
+        ## Need to deal with all cases of intersection:
+        ## 1. All 3 vertices on plane
+        ## 2. 2/3 vertices on plane
+        ## 3. middle vertex on plane
+        ## 4. top/bottom vertex on plane
+        
         for j, edge in enumerate(edges):
-            # Skip if the edge and plane are parallel
-            # TODO: May increase runtime
-            if np.dot(edge, plane_normal) == 0:
+            
+            # Skip edge if the edge and plane are parallel
+            if np.dot(plane_normal, edge) == 0:
                 continue
+            
             # Find the intersection of the edge and the plane
             t = np.dot(plane_normal, plane_point - verts[j]) / np.dot(plane_normal, edge)
-            if t >= 0 and t <= 1:
-                intersections.append(verts[j] + t * edge)
-        # If the "edge" has the start and end point at the same location, then it is just a point, so we can discard it   
-        # E.g. (1.5 0, 1.5 0) is a point, not an edge    
-        if intersections and not shapely.equals_exact(Point(intersections[0]), Point(intersections[1]), tolerance=1e-3): 
-            new_edge = intersections
+            if 0 <= t <= 1:
+                new_edge.append(verts[j] + t * edge)
+        if(len(new_edge)) > 2:
+                print("fuck")
+                print(new_edge)        
+        # If both points of intersections are too close together then ignore it
+        if new_edge and not shapely.equals_exact(Point(new_edge[0]), Point(new_edge[1]), tolerance=1e-4): 
+            
             new_edge_vector = np.array(new_edge[1]) - np.array(new_edge[0])
             cross_product = np.cross(new_edge_vector, normals[i])
             # TODO: May need to do normals[i] dot product with slicing plane to project normal onto plane before doing cross product
             # 
             if cross_product[2] < 0:
                 new_edge.reverse()
-            
             # Remove the Z values
-            for i, edge in enumerate(new_edge):
-                new_edge[i] = edge[:2]
-            #new_edge = shapely.set_precision(LineString(new_edge), 0.01)
-            new_edge = (LineString(new_edge))
-            polygon_edges.append(new_edge)
-    return shapely.set_precision(MultiLineString(polygon_edges), 0.01)
+            new_edge = [edge[:2] for edge in new_edge]
+            
+                
+            # Add the edge to the list of edges    
+            polygon_edges.append(LineString(new_edge))
+    
+    # At this point, we have a list of LineStrings that represent the edges of the cross-section:
+    # [<LINESTRING (-1   -1   , -1    0.8)>, 
+    #  <LINESTRING (-1    0.8 , -1    1  )>,
+    #  <LINESTRING (-1    1   ,  0.8  1  )>, 
+    #  <LINESTRING ( 0.8  1   ,  1    1  )>, 
+    #  <LINESTRING ( 1    1   ,  1   -0.8)>, 
+    #  <LINESTRING ( 1   -0.8 ,  1   -1  )>, 
+    #  <LINESTRING (-0.8 -1   , -1   -1  )>, 
+    #  <LINESTRING ( 1   -1   , -0.8 -1  )>]
+    
+    # Now we need to combine the edges into a single polygon
+    # <LINESTRING (-1   -1   , -1    0.8, -1    1  ,  0.8  1  ,  1    1  ,  1   -0.8,  1   -1  , -0.8 -1  , -1   -1  )>
+    # <POLYGON ((-1   -1   , -1    0.8, -1    1  ,  0.8  1  ,  1    1  ,  1   -0.8,  1   -1  , -0.8 -1  , -1   -1  ))>
+    
+    output_polys = []
+    #for edge in polygon_edges:
+    #    if len(list(edge.coords)) > 2:
+    #        print("fuck\n\n\n\n\n\n\n\n")
+    # Keep creating polygons until no more edges remain
+    num_edges = len(polygon_edges)
+    while(polygon_edges):
+        # Create a new LINESTRING starting at the first edge: 
+        # <LINESTRING (-1   -1   , -1    0.8)>
+        
+        
+        poly = polygon_edges[0]
+        
+        # Then delete the first edge from the list
+        polygon_edges.pop(0)
+        poly_creation_failed = False
+        # Repeat until the end of the LINESTRING is the same as the start of the LINESTRING
+        i=0
+        while(not shapely.equals_exact(Point(poly.coords[0]), Point(poly.coords[-1]), tolerance=1e-4)):
+            i+=1
+            
+            if i % 10 == 0:
+                print("Layer progress: ", round(100*(1-len(polygon_edges)/num_edges), -1), "%" , end='\r')
+            # Look for the next edge that starts at the end of the current edge and add the second coordinate to the LINESTRING
+            # <LINESTRING (-1   -1   , -1    0.8, -1    1  )>
+            edge_found = False
+            for j, next_edge in enumerate(polygon_edges):
+                #print(len(list(next_edge.coords)))
+                if shapely.equals_exact(Point(poly.coords[-1]), Point(next_edge.coords[0]), tolerance=1e-4):
+                    poly = LineString(list(poly.coords) + list(next_edge.coords[1:]))
+                    
+                    # Then delete the next edge from the list
+                    polygon_edges.pop(j)
+                    edge_found = True
+                    break
+                if shapely.equals_exact(Point(poly.coords[-1]), Point(next_edge.coords[1]), tolerance=1e-4):
+                    print(list(poly.coords[-2:]))
+                    print(list(next_edge.coords[:]))
+                    poly = LineString(list(poly.coords) + list(next_edge.coords[0:-1]))
+                    
+                    # Then delete the next edge from the list
+                    polygon_edges.pop(j)
+                    edge_found = True
+                    break    
+                
+            if not edge_found:
+                print("ERROR: Was not able to close polygon")
+                print(poly)
+                fig = plt.figure()
+                ax = fig.add_subplot()
+                ax.plot(*poly.xy, color='blue')
+                for line in polygon_edges:
+                    ax.plot(*line.xy, color='red')
+                ax.axis('equal')
+                plt.show()
+                poly_creation_failed = True
+                break
+                
+        #print(poly)        
+        if poly_creation_failed:
+            continue
+        else:
+            try:
+                output_polys.append(Polygon(poly))
+            except:
+                print("ERROR: Was not able to create polygon")
+                print(poly)
+                fig = plt.figure()
+                ax = fig.add_subplot()
+                ax.plot(*poly.xy, color='blue')
+                for line in polygon_edges:
+                    ax.plot(*line.xy, color='red')
+                ax.axis('equal')
+                plt.show()
+                continue
+    print("                                      ", end='\r')    
+        
+    # Return the polygon edges as a MultiLineString
+    # shapely.set_precision is used to remove floating point errors
+    return MultiPolygon(output_polys)
 
 def stl_get_height(mesh):
     vertices = np.array(mesh.v0)
@@ -110,20 +212,20 @@ def sort_tris(mesh, layers):
     num_tris = len(mesh.vectors)    
     
     # TODO: Optimize this    
-    # For each triangle, find the layer it intersects    
+    # For each triangle, find the layer it intersects   
     for i in range(num_tris):
         v1, v2, v3 = mesh.vectors[i]
         # Find the minimum and maximum Z value for the triangle
         triangle_zs = np.array([v1[2], v2[2], v3[2]])
-        min_z = np.min(triangle_zs)
-        max_z = np.max(triangle_zs)
+        triangle_min_z = np.min(triangle_zs)
+        triangle_max_z = np.max(triangle_zs)
         
         # Find the layers that the triangle intersects        
         for j, layer in enumerate(layers):
             
-            # The triangle intersects the layer if the triangle's minimum Z is 
-            # less than the layer's Z and maximum Z is greater than the layer's Z
-            if layer >= min_z and layer <= max_z:
+            # The triangle must intersect the plane if there is a point above and below the plane
+            if triangle_min_z <= layer <= triangle_max_z:
                 tri_buckets[j].append((v1, v2, v3))
                 normal_buckets[j].append(mesh.normals[i])
+                
     return tri_buckets, normal_buckets
